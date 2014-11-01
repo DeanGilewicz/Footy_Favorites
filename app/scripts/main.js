75ae295bf761f2345ac4d6cf45a9766ef0642894
Dean Gilewicz <gilewiczd@gmail.com>
2014-11-01 19:10:13 -0400

    updated fields

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,53 +1,67 @@
 // Model
-var Team = Backbone.Model.extend ({
+var Favorite = Backbone.Model.extend ({
 
   defaults: {
     name: '',
-    location: '',
-    division: '',
-    stadium: ''
+    team: 'no team',
+    player: 'only on weekends',
+    stadium: '',
+    league: 'top class'
   },
 
   initialize: function() {
-
+    var n = this.get('name');
+    var t = this.get('team');
+    console.log(n + ' has been added');
+    console.log(t + ' is the fav team');
   }
 
 });
 
 // Collection
-var Teams = Backbone.Collection.extend ({
-  model: Team,
+var Favorites = Backbone.Collection.extend ({
+  model: Favorite,
   url: 'http://tiy-atl-fe-server.herokuapp.com/collections/testft'
 });
 
 
-var all_teams = new Teams();
+var all_favorites = new Favorites();
 
 
-$('#footy_teams').on('submit', function(e){
+$('#add_selections').on('click', function(e){
 
   // Prevent the default action of our form submission
   e.preventDefault();
 
-  // Grab the name from the input
-  var team_name = $('#team_name').val();
+  console.log('button clicked');
 
-  // Create a new instane of our Team constructor (Backbone.Model)
-  var t = new Team({
-    name: team_name
+  // Grab the name (value) from each of the inputs
+  var user_name = $('#user_name').val();
+  var team_name = $('#team_name').val();
+  var player_name = $('#player_name').val();
+  var stadium_name = $('#stadium_name').val();
+  var league_name = $('#league_name').val();
+
+  // Create a new instane of our Favorite constructor (Backbone.Model)
+  var f = new Favorite({
+    name: user_name,
+    team: team_name,
+    player: player_name,
+    stadium: stadium_name,
+    league: league_name
   });
 
-  // Access our Collection and add our new instance (Team) to our collection
-  all_teams.add(t);
+  // Access our Collection and add our new instance (Favorites) to our collection
+  all_favorites.add(f);
 
-  // Save our Team - this looks for a URL field or a URL field in our Collection
+  // Save our Favorites - this looks for a URL field or a URL field in our Collection
   // and saves it to that URL using a simple POST method
-  t.save();
+  f.save();
 
-  console.log(t);
+  console.log(f);
 
   // Clear my form
-  $(this)[0].reset();
+  $('#user_favs')[0].reset();
 
 
 });
