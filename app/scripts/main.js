1b88c3fe1fd611cf305cfbb97502c1f4bcd11647
Dean Gilewicz <gilewiczd@gmail.com>
2014-11-03 13:53:46 -0500

    restructued and added views

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,63 +1,23 @@
-// Underscore Template
-var favTemplate = $('#resTemplate').html();
-var favRenderer = _.template(favTemplate);
-
-
-// Model
-var Favorite = Backbone.Model.extend ({
-
-  defaults: {
-    name: '',
-    team: 'test',
-    player: '',
-    stadium: '',
-    league: ''
-  },
-
-  initialize: function() {
-    var n = this.get('name');
-    var t = this.get('team');
-    var p = this.get('player');
-    var s = this.get('stadium');
-    var l = this.get('league');
-    console.log(n + ' likes ' + t + ', thinks ' + p + ' is really good, ' + s + 'is the best stadium, and ' + l + ' is the best league' );
-  }
-
-});
-
-var favList;
-var myServer = 'http://tiy-atl-fe-server.herokuapp.com/collections/testft';
-
-// Collection
-var Favorites = Backbone.Collection.extend ({
-  model: Favorite,
-  url: myServer
-});
-
-
+// create instance of Favorites Collection
 var all_favorites = new Favorites();
 
 
-// shows all entries made in the console
+// pull all of the favorites entries from our server
 all_favorites.fetch().done(function(){
-  console.log(all_favorites);
+
+  var favsView = new FavoritesView({
+    collection: all_favorites
+  });
+
+
 });
 
-//add all the current favorites to show in browser
-$.getJSON(myServer).done(function(data){
-          favList = data;
-          _.each(favList, function(favs) {
-              $('#result_list').append(favRenderer(favs));
-          });
-        });
 
 
 $('#add_selections').on('click', function(e){
 
   // Prevent the default action of our form submission
   e.preventDefault();
-
-  console.log('button clicked');
 
   // Grab the name (value) from each of the inputs
   var user_name = $('#user_name').val();
@@ -82,12 +42,8 @@ $('#add_selections').on('click', function(e){
   // and saves it to that URL using a simple POST method
   f.save();
 
-  console.log(f);
-
   // Clear my form
   $('#user_favs')[0].reset();
 
 
 });
-
-// Display data in the browser
