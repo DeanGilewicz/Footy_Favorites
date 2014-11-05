38b34e77652a9f8a4c77684c950d71a5e286dc03
Dean Gilewicz <gilewiczd@gmail.com>
2014-11-05 13:26:44 -0500

    added routes

---
+++ app/scripts/routers/app_router.js
@@ -0,0 +1,33 @@
+(function () {
+
+  App.Routers.AppRouter = Backbone.Router.extend({
+
+    initialize: function () {
+      // route the inital URL
+      Backbone.history.start();
+    },
+
+    routes: {
+      // sets the initial page to 'home'
+      '' : 'home',
+      // allows function to be ran so url can track unique id of getaways #/edit/id
+      'edit/:id' : 'editCoffee'
+    },
+    home: function () {
+      // ??
+      new App.Views.AddGetaway();
+
+      // ??
+      new App.Views.ListGetaway({ collection: App.getaways });
+    },
+
+    editCoffee: function (trip) {
+
+      var g = App.getaways.get(trip);
+
+      new App.Views.SingleGetaway({ coffee: g });
+    }
+
+  });
+
+}());
